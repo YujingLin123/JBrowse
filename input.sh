e0fb976521f69ceb62445434981e8ed3ede216f2
Yujing Lin <67212190+linyujing199488@users.noreply.github.com>
2022-01-19 12:41:21 +0800

    Update input.sh

--- input.sh
+++ input.sh
@@ -1,3 +1,7 @@
+####产生JBrowse的基因参考文件
+bgzip sars-cov2.fa
+samtools faidx sars-cov2.fa.gz
+
 ####产生JBrowse的注释文件
 gt gff3 -sortlines -tidy ITAG4.0_gene_models.gff > ITAG4.0_gene_models.sorted.gff
 bgzip ITAG4.0_gene_models.sorted.gff
